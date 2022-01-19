d84ef4f441dd226fed27f5903d189aa0a9dc3471
shimajima-eiji <shimajima-eiji@live.jp>
2022-01-19 23:43:47 +0900

    出力フォーマットを整える

--- for_Development/googletrans_test.py
+++ for_Development/googletrans_test.py
@@ -10,12 +10,12 @@ curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Operation-Maintenanc
 '''
 
 from googletrans import Translator
+from json import dumps
 
 try:
   tr = Translator(service_urls=['translate.googleapis.com'])
 except Exception as e:
   tr = Translator()
 
-result = tr.translate("テスト", src="ja", dest="en")
-print(result)
+print(dumps(tr.translate("テスト", src="ja", dest="en"), ensure_ascii=False))
 # 翻訳されたtestが表示されれば成功
