08fe958089d131e11b9c72607b2a2a75f7f86140
shimajima-eiji <shimajima-eiji@live.jp>
2022-01-19 23:23:03 +0900

    try-exceptのエラー解消し、ファイル名の競合回避

--- for_Development/googletrans_test.py
+++ for_Development/googletrans_test.py
@@ -1,13 +1,20 @@
 '''
+# 使用パッケージ
 pip install git+https://github.com/alainrouillon/py-googletrans@feature/enhance-use-of-direct-api
 pip install googletrans==4.0.0-rc1
+
+# 実行例
+```
+curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Operation-Maintenance/main/for_Development/googletrans_test.py | python
+```
 '''
+
 from googletrans import Translator
 
-try {
+try:
   tr = Translator()
-} catch {
+except Exception as e:
   tr = Translator(service_urls=['translate.googleapis.com'])
-}
+
 print(tr.translate("テスト", src="ja", dest="en").text)
 # 翻訳されたtestが表示されれば成功
