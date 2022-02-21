982a93a6c3e0010352927519f8344b8280663455
shimajima-eiji <shimajima-eiji@live.jp>
2022-02-21 17:36:34 +0900

    ドキュメンテーション機能を強化するスクリプトを追加

---
+++ for_Development/convert_shdoc.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# 引数にファイルパスを受け取っていない場合は処理をしない
+if [ "$#" -lt 1 -o ! -f "$1" ]
+then
+  echo "[Stop] Required 1 args.(file-path)"
+  echo ""
+  echo "[End $0]"
+  exit 1
+fi
+
+echo "[Start ${0}]"
+echo ""
+
+write_flag="false"
+source_file=$1
+markdown_file="${source_file%.*}.md"
+
+# 既にマークダウンファイルが存在する場合は削除しておく
+if [ -f "${markdown_file}" ]
+then
+  rm ${markdown_file}
+fi
+
+# ファイルの行数を取得し、行数分ループする
+for index in $(seq 1 $(sed -n '$=' ${source_file}))
+do
+  line="$(sed -n ${index}P ${source_file})"
+
+  # READMEの書き込み開始地点を検出
+  if [ "${write_flag}" = "false" -a -n "$(echo ${line} | grep '<<README')" ]
+  then
+    write_flag="true"
+  
+  # READMEの書き込み終了地点を検出
+  elif [ "${write_flag}" = "true" -a -n "$(echo ${line} | grep 'README')" ]
+  then
+    write_flag="false"
+
+  # 書き込み範囲内の場合、追記
+  elif [ "${write_flag}" = "true" ]
+  then
+    # コード部分はエスケープのため、「#``」としているのでこれを正しく表示させる
+    if [ "${line}" = '#``' ]
+    then
+      line='```'
+    fi
+
+    echo "${line}" >>${markdown_file}
+
+  # 書き込み範囲外の場合、何もせず終了
+  fi
+done
+
+echo "Convert [${source_file}] -> [${markdown_file}]"
+echo ""
+echo "[Complete $0]"
---
+++ for_Development/setup_develop-code.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+: <<README
+# setup_develop-code.sh
+## 使い方
+当該コード内に以下を入れておく
+
+#``
+curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Operation-Maintenance/main/for_Development/setup_develop-code.sh >./setup_develop-code.sh
+source ./setup_develop-code.sh
+rm ./setup_develop-code.sh
+#``
+
+## 解説
+- shebangは意識する必要はないが、./setup_develop-code.shで/bin/shが実行できるように指定しておく
+- 敢えて白色を指定するシーンが思いつかないので、関数ではなく定数とする。なくてもいい
+README
+
+# 定義
+__color="\033"
+__white="${__color}[37m"
+__print() {
+  printf "${__color}$1${@:2}${__white}"
+}
+
+# 使用したいカラーコードは以下に追加する
+__red() {
+  __print "[31m" "$@"
+}
+
+__blue() {
+  __print "[34m" "$@"
+}
+
+__yellow() {
+  __print "[33m" "$@"
+}
+
+## 実行シェルをシバンで強制したい場合
+__search_shell() {
+  if [ $# -lt 1 ]
+  then
+    echo "[$(__red Stop): $(__blue __search_shell)] Required 1 args.(path)"
+    exit 1
+  fi
+
+  # Macの場合
+  if [ $(uname) = "Darwin" ]
+  then
+    run_shell="$(ps $$ | awk 'NR==2 {print $5}' | awk -F/ '{print $NF}')"
+
+  # スクリプト側が対応していないケースを想定して、手入力も許可する
+  else
+    run_shell=$2
+  
+  fi
+  shebang="$(head -1 $1 | awk -F/ '{print $NF}')"
+
+  if [ ! "${run_shell}" = "${shebang}" ]
+  then
+    echo "[$(__red Stop): $(__blue __search_shell)] Required command: [ $(__blue ./${1}) ]"
+    exit
+  fi
+}
