75dd6021ab8ce19fd076d13a794d05d276c889ae
shimajima-eiji <shimajima-eiji@live.jp>
2022-02-25 07:44:32 +0900

    スクリプトロジックを短縮化・最適化

--- for_Development/setup_develop-code.sh
+++ for_Development/setup_develop-code.sh
@@ -6,12 +6,14 @@
 当該コード内に以下を入れておく
 
 #``
+# 開発ツールをインストール
 if [ ! "$(type -t __check_setup_develop_code)" = "function" ]
 then
   curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Operation-Maintenance/main/for_Development/setup_develop-code.sh >./setup_develop-code.sh
   source ./setup_develop-code.sh
   rm ./setup_develop-code.sh
 fi
+# インストールに失敗していたら、以下の実行に失敗してしまう
 #``
 
 ## 解説
@@ -26,78 +28,34 @@ fi
 
 README
 
-# 判定ロジック用。変更しない
-__check_setup_develop_code() {
-  echo "version: 2022.02.24"
-}
+# 判定ロジック用のため名前を変えない。せめてバージョンチェックをできるようにしておく
+__check_setup_develop_code() { echo "version: 2022.02.24"; }
 
 # 定義
 __color="\033"
 __white="${__color}[37m"
-__print() {
-  printf "${__color}$1${@:2}${__white}"
-}
+__print() { printf "${__color}$1${@:2}${__white}"; }
 
 # 使用したいカラーコードは以下に追加する
-__red() {
-  __print "[31m" "$@"
-}
-
-__green() {
-  __print "[32m" "$@"
-}
-
-__blue() {
-  __print "[34m" "$@"
-}
-
-__yellow() {
-  __print "[33m" "$@"
-}
+__red() { __print "[31m" "$@"; }
+__green() { __print "[32m" "$@"; }
+__blue() { __print "[34m" "$@"; }
+__yellow() { __print "[33m" "$@"; }
 
 ## よく使うカラーコードを利用するパターンを登録しておく
-__start() {
-  echo "[$(__blue Start): $1]"
-  echo ""
-}
-
-__skip() {
-  echo "[$(__yellow Skip)] $1"
-}
-
-__end() {
-  echo ""
-  echo "[$(__blue End: ): $1]"
-  exit 0
-}
-
-__success() {
-  echo "[$(__green Success)] $1"
-}
+__start() { echo "[$(__blue Start): $1]\n"; }
+__skip() { echo "[$(__yellow Skip)] $1"; }
+__success() { echo "[$(__green Success)] $1"; }
+__end() { echo "\n[$(__blue End: ): $1]"; exit 0; }
+__stop() { echo "[$( __red Stop ): $( __blue "$1" )] '$2'"; __end "$3"; }
 
 # 実行シェルをシバンで強制したい場合
 __search_shell() {
-  if [ $# -lt 1 ]
-  then
-    echo "[$(__red Stop): $(__blue __search_shell)] Required 1 args.(path)"
-    exit 1
-  fi
+  # 変数チェック。ファイルパスでなければやらない
+  [ -f "$1" ] && __stop "__search_shell" "Required 1 args.(path)"
 
-  # Macの場合
-  if [ $(uname) = "Darwin" ]
-  then
-    run_shell="$(ps $$ | awk 'NR==2 {print $5}' | awk -F/ '{print $NF}')"
-
-  # スクリプト側が対応していないケースを想定して、手入力も許可する
-  else
-    run_shell=$2
-  
-  fi
+  # 実行シェルとシバンが一致しない場合は実行しない
+  run_shell="$(ps $$ | awk 'NR==2 {print $5}' | awk -F/ '{print $NF}')"
   shebang="$(head -1 $1 | awk -F/ '{print $NF}')"
-
-  if [ ! "${run_shell}" = "${shebang}" ]
-  then
-    echo "[$(__red Stop): $(__blue __search_shell)] Required command: [ $(__blue ./${1}) ]"
-    exit
-  fi
+  [ ! "${run_shell}" = "${shebang}" ] && __stop "__search_shell" "Required command: [$(__blue "./$1")]"
 }
