c344959768e83ac95c643628101eaf9918d6364e
shimajima-eiji <shimajima-eiji@live.jp>
2022-02-25 12:47:34 +0900

    [Update] convert_webp: パスを取得し、ファイルでもディレクトリでも対応する

--- for_github/workflows/convert_webp.sh
+++ for_github/workflows/convert_webp.sh
@@ -64,25 +64,65 @@ convert_file() {
 
 : <<__main__
 pngとjpgをwebpに変換する
-元ファイルはbaseディレクトリへ、webpファイルはwebpディレクトリに移動する。
+パスがディレクトリの場合、元ファイルはbaseディレクトリへ、webpファイルはwebpディレクトリに移動する
 既にwebpに変換している場合はスキップ
 __main__
-for file in $(find "$(dirname $0)" -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -not -name "*/base/*")
+path=${1:-$(dirname $0)}
+
+# パスがファイルなら、そのファイルだけ変換
+# ディレクトリの場合と違って、webpファイルがあっても上書きする
+if [ -f "${path}" ]
+then
+  # 拡張子がpng, jpgでなければやらない
+  extension=${path##*.}
+  if [ ! "${extension}" = "png" ] && [ ! "${extension}" = "jpg" ] && [ ! "${extension}" = "jpeg" ]
+  then
+    __skip "Not supported extension: [${extension}]"
+    __end $0
+  fi
+
+  current="$(dirname ${path})"
+  webp="${current}/$(basename ${path%.*}.webp)"
+  convert_file "${path}" "${webp}" "${current}"
+  exit 0
+
+# パスがファイルでもディレクトリでもない場合はスキップ
+elif [ ! -d "${path}" ]
+then
+  __skip "Not found: [${path}]"
+  __end "$0"
+fi
+
+# パスがディレクトリの場合、ファイルサーチ
+# baseディレクトリのファイルは対象外とする
+# ファイルの場合と違って、webpファイルが存在していた場合はスキップ
+for file in $(find "${path}" -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -not -name "*/base/*")
 do
-  current="$(dirname ${file})"
   # baseディレクトリに移動したファイルは、変換をしているはずなので除外する
+  current="$(dirname ${file})"
   [ "$(basename ${current})" = "base" ] && continue
 
-  convert_to="${current}/webp"
-  webp="${convert_to}/$(basename ${file%.*}.webp)"
-
-  # webpディレクトリに存在しているファイル名は変換済みなので除外する
+  # webpファイルが同一ディレクトリ内に存在していれば変換をしているはずなので除外する
+  # ファイルを変換した後で、ディレクトリを指定しているケースを想定
+  webp="${file%.*}.webp"
   if [ -f "${webp}" ]
   then
     __skip "$(__green ${file}): already webp."
     continue
   fi
 
+  convert_to="${current}/webp"
+  webp="${convert_to}/$(basename ${file%.*}.webp)"
+
+  # webpディレクトリに存在しているファイル名は変換済みなので除外する
+  # ディレクトリ指定後にもう一度ディレクトリを指定したケースを想定
+  if [ -f "${webp}" ]
+  then
+    __skip "$(__green ${file}): already webp."
+    continue
+  fi
+
+  # 以下、変換実施。baseとwebpディレクトリを作成する
   convert_from="${current}/base"
   mkdir -p "${convert_from}"
   mkdir -p "${convert_to}"
