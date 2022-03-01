c47fa071b928a52ce42f8e0ef314b259a92f0c50
中　翔 <shimajima-eiji@live.jp>
2022-03-01 13:50:13 +0900

    Update curl_tester.py

--- for_Development/curl_tester.py
+++ for_Development/curl_tester.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-import sys
-
 """
 curlで動いているかどうかを調べるためのテスター
 curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Operation-Maintenance/main/curl_tester.py | python
 """
 
-if sys.version_info.major == 2:
-  print __file__
-elif sys.version_info.major == 3:
-  print(__file__)
+print __file__
